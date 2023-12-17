ea405175b3ea66b79ef9bad9244adb7b57e24b9c
HoneyBebra <128954566+HoneyBebra@users.noreply.github.com>
2023-12-17 19:49:06 +1000

    Added views products-list and product-detail

--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Category(models.Model):
@@ -19,6 +20,12 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        return reverse(
+            'shop:product_list_by_category',
+            args=[self.slug]
+        )
+
 
 class Product(models.Model):
     category = models.ForeignKey(
@@ -30,14 +37,14 @@ class Product(models.Model):
     slug = models.SlugField(max_length=200)
     image = models.ImageField(
         upload_to='products/%Y/%m/%d',
-        blank=True  # blank = optional
+        blank=True  # blank = optional field
     )
     description = models.TextField(blank=True)
     price = models.DecimalField(
         max_digits=10,
         decimal_places=2
     )
-    available = models.BooleanField(default=True)
+    available = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now_add=True)
 
@@ -51,3 +58,9 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse(
+            'shop:product_detail',
+            args=[self.id, self.slug]
+        )
