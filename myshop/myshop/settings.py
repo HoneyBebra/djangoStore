80ec9a37eb6f496846804ffca9b4e03241824c00
HoneyBebra <128954566+HoneyBebra@users.noreply.github.com>
2023-12-24 16:18:29 +1000

    Added orders app and models

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'shop.apps.ShopConfig',
-    'cart.apps.CartConfig'
+    'cart.apps.CartConfig',
+    'orders.apps.OrdersConfig'
 ]
 
 MIDDLEWARE = [
